e30b2211990a37db034dc323298d75a9b8ebd849
ramyses <lukasramysesleao@gmail.com>
2023-05-31 10:28:56 -0400

    :construction: Repet print screen logo alura
    create function for repeet 6 times components

--- main.js
+++ main.js
@@ -3,10 +3,21 @@ import './src/styles/settings/colors.css'
 import './src/styles/elements/base.css'
 
 import CardGame from "./src/components/CardGame"
+import printOnScreen6Times from './src/components/CardGame'
 
 const $root = document.querySelector("#root")
 const $htmlCardGame = CardGame()
+function setRepetPrint(){
+    for(let i = 0; i < 6; i++ ){
+        $root.insertAdjacentHTML("beforeend", CardGame())
+    }
+}
+
+setRepetPrint()
 
 
-
-$root.insertAdjacentHTML("beforeend", $htmlCardGame)
+//desafio 1
+//  6 * card alura
+//compónente player
+//todos os cards que faltam
+//fazer a carta virar
