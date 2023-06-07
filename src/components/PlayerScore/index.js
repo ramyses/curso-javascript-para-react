82fc6016b9dcb22462e1fb98e08186d21c3f77a0
ramyses <lukasramysesleao@gmail.com>
2023-06-06 22:19:17 -0400

    :lipstick: Create syles of the PlayerScore

--- src/components/PlayerScore/index.js
+++ src/components/PlayerScore/index.js
@@ -2,10 +2,10 @@ import './style.css'
 
 function PlayerScore(points = 0) {
     return /* html */`
-        <ol data-points="${points}">
-            <li>um</li>
-            <li>dois</li>
-            <li>tres</li>
+        <ol class="player-score" data-points="${points}">
+            <li class="pointer"></li>
+            <li class="pointer"></li>
+            <li class="pointer"></li>
         </ol>
     `; 
 }
